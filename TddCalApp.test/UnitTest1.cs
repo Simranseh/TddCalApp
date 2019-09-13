8ed79fbc4c74effb3cee9a3956b40bbc3d5bf275
Simranseh <ssimransehrawat.28@gmail.com>
2019-09-12 22:08:55 -0700

    Unit test 1 failed

---
+++ TddCalApp.test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalApp.library;
+
+namespace TddCalApp.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnPositivePassingTwoPositiveNumbers()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = 3;
+            int num2 = 2;
+            int result;
+            result = testCalculator.Add(num1, num2);
+            Assert.AreEqual(5, result, "Testing two Integer 3 and 2");
+        }
+    }
+}
