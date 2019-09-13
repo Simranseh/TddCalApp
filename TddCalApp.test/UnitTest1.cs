e7988663d0db5fe426593c8c3544966cd25b483d
Simranseh <ssimransehrawat.28@gmail.com>
2019-09-12 22:16:49 -0700

    Unit test 2 failed

--- TddCalApp.test/UnitTest1.cs
+++ TddCalApp.test/UnitTest1.cs
@@ -17,5 +17,16 @@ namespace TddCalApp.test
             result = testCalculator.Add(num1, num2);
             Assert.AreEqual(5, result, "Testing two Integer 3 and 2");
         }
+        [TestMethod]
+        public void ShouldReturnZeroOnPassingPairOfPositiveandNegativeNumbers()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = -3;
+            int num2 = +2;
+            int result;
+            result = testCalculator.Add(num1, num2);
+            Assert.AreEqual(0, result, " Testing pair of positive and negative numbers ");
+        }
     }
+
 }
